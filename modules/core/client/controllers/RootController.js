c0888164381357d1299030d82c92901643796d6b
flareair <flareair0@gmail.com>
2016-03-21 11:49:58 +0300

    Added logout option

--- modules/core/client/controllers/RootController.js
+++ modules/core/client/controllers/RootController.js
@@ -4,12 +4,20 @@ angular
     .module('App')
     .controller('RootCtrl', RootCtrl);
 
-RootCtrl.$inject = ['authService'];
+RootCtrl.$inject = ['authService', '$window', '$location'];
 
-function RootCtrl(authService) {
+function RootCtrl(authService, $window, $location) {
     console.log(authService.username);
     this.username = function() {
         return authService.username;
+    };
+
+    this.logOut = function() {
+        authService.isLogged = false;
+        authService.username = null;
+        delete $window.sessionStorage.token;
+        delete $window.sessionStorage.username;
+        $location.path('/');
     };
 }
 
